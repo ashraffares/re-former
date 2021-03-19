47e8939b22ad0f1c8814ea473210e0121ddc0caf
ashraffares <ashraffares090@gmail.com>
2021-03-19 17:40:03 +0200

    Add Edit view and edit method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @user = User.new 
   end
 
+  def show; end
+
+  def edit
+  end
+
   def create
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
@@ -13,9 +18,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def user_params
     params.require(:users).permit(:username, :email, :password)
+  end
+
+  
+  def set_user
+    @article = User.find(params[:id])
   end
 end
